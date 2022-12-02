b3f2d2c260c363ac94451828b1bad0ec74f4040f
@ethkimchhay03 <ethkimchhay03@gmail.com>
2022-12-02 22:05:46 +0700

    add

--- app.js
+++ app.js
@@ -8,13 +8,8 @@ const postsRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categorys");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const corsOptions = {
-  origin: "*",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
-};
 
-app.use(cors(corsOptions)); 
+app.use(cors()); 
 app.use(bodyParser.json());
 app.use("/posts", postsRoute);
 app.use("/categories", categoryRoute);
